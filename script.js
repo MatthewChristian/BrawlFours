5ea21986011e3a885fce521c85127202a695a910
MatthewChristian <matthewjlchristian@yahoo.com>
2020-03-11 06:32:12 +0000

    Changed team1Score and team2Score into an array named "score"

--- script.js
+++ script.js
@@ -55,12 +55,34 @@ function dealAll(player,deck) {
   }
 }
 
-function checkGame(t1,t2) { //Returns 1 if Team 1 won, 2 if Team 2 won, 0 if nobody has won yet
-  if (t1 >= 12)
+function checkGame(score) { //Returns 1 if Team 1 won, 2 if Team 2 won, 0 if nobody has won yet
+  if (score[0] >= 12)
     return 1;
-  if (t2 >= 12)
+  if (score[1] >= 12)
     return 2;
   return 0;
+}
+
+function checkKicked(kicked,score) {
+  if (kicked.Value == 6) {
+    if (dealer == 1 || dealer == 3)
+      score[0]+=2;
+    else
+      score[1]+=2;
+  }
+  if (kicked.Value == "J") {
+    if (dealer == 1 || dealer == 3)
+      score[0]+=3;
+    else
+      score[1]+=3;
+  }
+  if (kicked.Value == "A") {
+    if (dealer == 1 || dealer == 3)
+      score[0]++;
+    else
+      score[1]++;
+  }
+  return score;
 }
 
 function mainGame(dealer) {
@@ -70,8 +92,9 @@ function mainGame(dealer) {
     player[i] = new Hand();
   }
   let game=0;
-  let team1Score = 0;
-  let team2Score = 0;
+  let score = [];
+  score[0] = 0; //Team 1 Score
+  score[1] = 0; //Team 2 Score
   dealAll(player,deck);
   let kicked=deck.pop();
   for (var i=0; i<4; i++) {
@@ -79,28 +102,12 @@ function mainGame(dealer) {
   }
   console.log("Trump is:", kicked.Suit);
   console.log(kicked);
-  if (kicked.Value == 6) {
-    if (dealer == 1 || dealer == 3)
-      team1Score+=2;
-    else
-      team2Score+=2;
-  }
-  if (kicked.Value == "J") {
-    if (dealer == 1 || dealer == 3)
-      team1Score+=3;
-    else
-      team2Score+=3;
-  }
-  if (kicked.Value == "A") {
-    if (dealer == 1 || dealer == 3)
-      team1Score++;
-    else
-      team2Score++;
-  }
-  console.log(team1Score);
-  game=checkGame(team1Score, team2Score);
+  score=checkKicked(kicked,score);
+  console.log(score[0]);
+  game=checkGame(score);
   return game;
 }
+
 
 let game=0;
 let dealer=1;
